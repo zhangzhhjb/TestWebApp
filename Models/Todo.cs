5e5f5f197a6fc135454be839c458726b7baf0b1a
zhang zhihong <zhangzh@tk.totyu.com>
2018-06-17 15:03:08 +0800

    added done field

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -11,6 +11,8 @@ namespace DotNetCoreSqlDb.Models
         public int ID { get; set; }
         public string Description { get; set; }
 
+        public bool Done { get; set; }
+
         [Display(Name = "Created Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
